086b465dc49f87d65ef55cf2461cdc6fa43c7b0a
Curry <66020230+curryducker@users.noreply.github.com>
2024-03-18 16:03:56 +0100

    poesj

--- common/src/main/java/nl/teamdiopside/cutteryeet/mixin/EntityMixin.java
+++ common/src/main/java/nl/teamdiopside/cutteryeet/mixin/EntityMixin.java
@@ -4,17 +4,20 @@ import dev.architectury.platform.Platform;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StonecutterBlock;
-import net.minecraft.entity.Attackable;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
+import net.minecraft.entity.vehicle.BoatEntity;
 import net.minecraft.registry.Registries;
+import net.minecraft.server.command.CommandOutput;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Nameable;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.World;
+import net.minecraft.world.entity.EntityLike;
 import nl.teamdiopside.cutteryeet.CutterYeet;
 import nl.teamdiopside.cutteryeet.config.Config;
 import org.spongepowered.asm.mixin.Mixin;
@@ -23,33 +26,31 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LivingEntity.class)
-public abstract class LivingEntityMixin extends Entity implements Attackable {
-    protected LivingEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
-        super(entityType, world);
-    }
+@Mixin(Entity.class)
+public abstract class EntityMixin implements Nameable, EntityLike, CommandOutput {
 
-    @Inject(method = "tickMovement", at = @At("HEAD"))
+    @Inject(method = "tick", at = @At("HEAD"))
     public void tickMovement(CallbackInfo ci) {
-        if ((LivingEntity) (Object) this instanceof PlayerEntity player && player.getAbilities().flying) {
+        Entity self = (Entity) (Object) this;
+        if ((self instanceof PlayerEntity player && player.getAbilities().flying) || !(self instanceof LivingEntity || self instanceof BoatEntity || self instanceof ItemEntity)) {
             return;
         }
 
-        BlockState blockState = getBlockStateAtPos();
-        if (!isSneaking() && isStonecutter(blockState)) {
+        BlockState blockState = self.getBlockStateAtPos();
+        if (!self.isSneaking() && cutterYeet$isStonecutter(blockState)) {
             // Fixes inconsistencies
-            this.setOnGround(false);
+            self.setOnGround(false);
 
             Direction direction = blockState.get(Properties.HORIZONTAL_FACING);
-            Vec3d pushVelocity = Vec3d.of(direction.rotateCounterclockwise(Direction.Axis.Y).getVector()).add(0, 0.3, 0).multiply(Config.yeetStrength);
-            this.setVelocity(pushVelocity);
+            Vec3d pushVelocity = Vec3d.of(direction.rotateCounterclockwise(Direction.Axis.Y).getVector()).add(0, 0.3, 0).multiply(self instanceof ItemEntity ? 0.65 * Config.yeetStrength : Config.yeetStrength);
+            self.setVelocity(pushVelocity);
 
-            this.playSound(CutterYeet.CUT_SOUND.get(), 1f, 1f);
+            self.playSound(CutterYeet.CUT_SOUND.get(), 1f, 1f);
         }
     }
 
     @Unique
-    public boolean isStonecutter(BlockState blockState) {
+    public boolean cutterYeet$isStonecutter(BlockState blockState) {
         boolean isInsideModdedCutter = false;
         if (Platform.isModLoaded("corail_woodcutter")) {
             Block block = Registries.BLOCK.get(new Identifier("corail_woodcutter", "oak_woodcutter"));
